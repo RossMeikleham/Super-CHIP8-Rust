a0c5808213e88cb56af57bf8e5efbb3bc4ef5679
RossIain <rossmeikleham@hotmail.co.uk>
2014-06-09 14:27:38 +0100

    Updated main so loaded programs from memory start at address 0x200 which each program should start at hardcoded address 0x200 due to the original VM spec.

--- main.rs
+++ main.rs
@@ -1,9 +1,10 @@
 use std::io::File; /* input/output */
 use std::string::String;
-
 use std::os;
 
-static MAX_RAM : uint = 4096;
+
+static MAX_RAM : uint = 0xFFF;
+static START_RAM :uint = 0x200;
 
 /* Reads a ROM file and returns a vector containing
  * its bytes if successful. Otherwise an error string
@@ -11,16 +12,21 @@ static MAX_RAM : uint = 4096;
 fn read_rom(file_path: String) -> Result<Vec<u8>,String> {
     
     match File::open(&Path::new(file_path)).read_to_end() {
-        Ok(mem) =>  {
+
+        Ok(rom_contents) =>  {
+            /* Programs start at address 0x200, in original implementation
+             * 0x000 - 0x1FF reserved for VM, just pad start of mem with 
+             * 0s in this case */
+            let mem = Vec::from_elem(START_RAM, 0u8) + rom_contents;
 
             match mem.len() {
             /* Pad end of memory until max memory capacity reached with 0s */
                 s if s <=  MAX_RAM  => 
-                    Ok(mem + Vec::from_elem(MAX_RAM - s, 0u8)),
-            /* Memory read in is too large */
+                    Ok(mem + Vec::from_elem(MAX_RAM -  s, 0u8)),
+            /* Memory read in from game ROM is too large */
                 s  =>   Err(format!("game image is too large 
                     ({} bytes), must be a maximum of {} bytes"
-                    ,s , MAX_RAM).to_string()),
+                    ,s , MAX_RAM - START_RAM).to_string()),
             }
         },
         /* Error reading file */
