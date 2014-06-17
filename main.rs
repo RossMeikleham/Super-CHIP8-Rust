5a214e0b5ed00bb9d8481adf8f616c927f8bf766
RossIain <rossmeikleham@hotmail.co.uk>
2014-06-17 14:34:40 +0100

    Added timings for controlling speed of the emulator

--- main.rs
+++ main.rs
@@ -1,14 +1,18 @@
+extern crate time;
+
 use std::io::File; /* input/output */
 use std::string::String;
 use std::os;
 use core::CPU;
+use std::io::timer;
 
 mod core;
 
 static MAX_RAM : uint = 0xFFF;
 static START_RAM :uint = 0x200;
 
-
+static INSTRUCTIONS_PER_SEC : u64 = 500;
+static CYCLES_CHECK : u64 = 20;
 
 /* Reads a ROM file and returns a vector containing
  * its bytes if successful. Otherwise an error string
@@ -37,7 +41,26 @@ fn read_rom(file_path: String) -> Result<Vec<u8>,String> {
     }
 } 
 
+fn get_time_ms() -> u64 { time::precise_time_ns()/1000000 }
 
+fn wait_for_next_cycle(old_time:u64, instructions:u64, ins_per_sec:u64 )  {
+    let current_time = get_time_ms();
+    if old_time < current_time {
+        timer::sleep(((current_time - old_time) * instructions) / ins_per_sec);
+    }
+}
+
+
+fn run_program(mut chip8 :core::CPU, cycle_max: u64, ins_per_sec: u64)  {
+    
+    loop {
+        let start_timer = get_time_ms();
+        for _ in range(0, cycle_max) {
+            chip8.perform_cycle();
+        }
+        wait_for_next_cycle(start_timer, cycle_max, ins_per_sec);
+    }
+}
 
 
 fn main() {
@@ -57,11 +80,7 @@ fn main() {
     
     assert!(memory.len() <=  MAX_RAM);
 
-    let mut chip8 = core::CPU::new(memory);
-    loop {
-    //for i in range(0, 51) {
-        chip8.perform_cycle();
-    }
+    run_program(core::CPU::new(memory), CYCLES_CHECK, INSTRUCTIONS_PER_SEC);
 }
 
 
