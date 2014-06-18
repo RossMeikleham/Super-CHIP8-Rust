54e3d9d21528eef1caecdaa639ac6160a1097ebe
RossIain <rossmeikleham@hotmail.co.uk>
2014-06-18 05:29:24 +0100

    Added tests for subtraction and adding to index register

--- core_tests.rs
+++ core_tests.rs
@@ -1,6 +1,8 @@
 use core::CPU;
 mod core;
 
+
+
 fn setup_blank_cpu() -> CPU {
     CPU::new(Vec::from_elem(0x1, 0u8))
 }
@@ -32,17 +34,16 @@ fn check_hex_to_digits_converter() {
 }
 
 
-/*#[test]
+#[test]
 fn check_add_regs() {
-    let mut cpu = setup_blank_cpu();
-    let mut cpu2 = setup_blank_cpu();
+    let mut cpu = setup_blank_cpu();    
     cpu.interpret(0x6205); /* Load 0x5 into reg 2 */
     cpu.interpret(0x6510); /* Load 0x10 into reg 5 */
     cpu.interpret(0x8254); /* Add reg 5 to reg 2 and store in reg 2 */
-    cpu2.interpret(0x6205);
+    
     assert_eq!(cpu.get_reg(0xF), 0x0);
     assert_eq!(cpu.get_reg(2), 0x15);
-}*/
+}
 
 #[test]
 fn check_add_regs_overlow_value() {
@@ -54,6 +55,108 @@ fn check_add_regs_overlow_value() {
     assert_eq!(cpu.get_reg(0xF),  0x1); /* Check flag set */
     assert_eq!(cpu.get_reg(1), 0xFE); 
 }
+
+/*** Test opcode 8XY5 ***/
+
+/* Check a subtraction which results
+ * in a value greater than zero */
+#[test]
+fn check_sub_regs() {
+    let mut cpu = setup_blank_cpu();
+    cpu.interpret(0x64F0); /* Load F0 into reg 4 */
+    cpu.interpret(0x6301); /* load 1 into reg 3 */
+    cpu.interpret(0x8435); /* sub reg 3 from reg 4 and store in reg 4 */
+
+    assert_eq!(cpu.get_reg(0xF), 0x1); /* check no overflow */
+    assert_eq!(cpu.get_reg(4), 0xF0 - 0x1);
+
+}
+
+/* Check a subtraction which results
+ * in a values less than zero */
+#[test]
+fn check_sub_regs_overflow() {
+    let mut cpu = setup_blank_cpu();
+    cpu.interpret(0x6712); /* Load 0x12 into reg 7 */
+    cpu.interpret(0x6920); /* Load 020 into reg 9 */
+    cpu.interpret(0x8795); /* sub r9 from r7 store result in r7 */
+
+    assert_eq!(cpu.get_reg(0xF), 0x0); /* check overflow */
+    assert_eq!(cpu.get_reg(0x7), (0x12 - 0x20) as u8);
+
+}
+
+
+/*** Test opcode 8XY7, subbing registers ***
+ *** the other way round than from 8XY5  ***/
+
+
+/* Check a subtraction which results
+ * in a value greater than zero */
+#[test]
+fn check_sub_inverted_regs() {
+    let mut cpu = setup_blank_cpu();
+    cpu.interpret(0x6A23);
+    cpu.interpret(0x6B30);
+    cpu.interpret(0x8AB7);
+
+    assert_eq!(cpu.get_reg(0xF), 0x1);
+    assert_eq!(cpu.get_reg(0xA), 0x30 - 0x23);
+}
+
+
+
+/* Check a subtraction which results
+ * in a value less than zero */
+#[test]
+fn check_sub_inverted_regs_overflow() {
+    let mut cpu = setup_blank_cpu();
+    cpu.interpret(0x6CD3);
+    cpu.interpret(0x6E30);
+    cpu.interpret(0x8CE7);
+
+    assert_eq!(cpu.get_reg(0xF), 0x0);
+    assert_eq!(cpu.get_reg(0xC), (0x30 - 0xD3) as u8); 
+}
+
+
+
+#[test]
+fn check_index_add_reg() {
+    let mut cpu = setup_blank_cpu();
+    cpu.interpret(0xA111); //set I to 0x111
+    cpu.interpret(0x6120); // set reg 1 to 20
+    cpu.interpret(0xF11E); // Add reg 1 to I
+
+    assert_eq!(cpu.get_reg(0xF), 0x0);
+    assert_eq!(cpu.get_index_reg(), 0x20 + 0x111);
+}
+
+
+
+#[test]
+fn check_index_add_reg_overflow() {
+    let mut cpu = setup_blank_cpu();
+    cpu.interpret(0xAFFF); //set I to 0xFFF
+    cpu.interpret(0x6C56); // set reg C to 0x56
+    cpu.interpret(0xFC1E); // Add reg C to I
+
+    assert_eq!(cpu.get_reg(0xF), 0x1);
+    assert_eq!(cpu.get_index_reg(), (0xFFF + 0x056) % 0x1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
